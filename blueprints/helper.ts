52bcfb7fe7b49cd2f3aad6043ebea3a4297011f4
Nick Thesing <nickthesing@gmail.com>
2021-09-27 20:47:32 +0200

    Feat(blueprints): added all blueprints used by the scripts

---
+++ blueprints/helper.ts
@@ -0,0 +1,72 @@
+
+/*
+* Smart Mirror Widget
+* 
+* [ .. name here .. ]
+* [ .. short description here .. ]
+*/
+
+class Name extends WidgetHelper {
+
+  /**
+   * name of the widget
+   * 
+   * @type {string}
+   * @public
+  */
+  name: string = "Name";
+
+  /**
+   * @constructor
+   * 
+   * @param {any} data - widget data
+  */
+  constructor(data: any) {
+    super(data);
+
+    this._addSocketListener();
+  }
+
+  /**
+   * @_addSocketListener
+   * 
+   * Put your socket listeners in this method.
+   * 
+   * @private
+  */
+  _addSocketListener() { }
+
+  /**
+   * @afterStart
+   * 
+   * Method to be called after the widget is started.
+   * 
+   * @param {any} widgetData - full widget settings object
+   * @public
+  */
+  afterStart(widgetData: any) {
+    console.log(`Widget Data: ${this.name}`, widgetData);
+  }
+
+  /**
+   * @afterStop
+   * 
+   * Method to be called after the widget is stopped.
+   * 
+   * @param {any} widgetData - full widget settings object
+   * @public
+  */
+  afterStop() { }
+
+  /**
+   * @afterReload
+   * 
+   * Method to be called after the widget is reloaded.
+   * 
+   * @param {any} widgetData - full widget settings object
+   * @public
+  */
+  afterReload() { }
+}
+
+export default Name;
